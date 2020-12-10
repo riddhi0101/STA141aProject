c8cd2f158f05129812c18ea7dd94d91e4120dbe3
riddhi0101 <rjbarbhaiya@ucdavis.edu>
2020-12-09 16:44:56 -0800

    new code-slusters

--- subGroupClustering.R
+++ subGroupClustering.R
@@ -1,5 +1,5 @@
 ## grouping clustering
-
+library("scatterplot3d")
 library(dplyr)
 library(kernlab)
 library(cluster)
@@ -25,10 +25,9 @@ if(exists('travel_review'))
 if(exists('error_lines'))
     rm(error_lines)
 
-# check to see that all observations are complete
-sum(!complete.cases(review_data))
-#pairs(review_data[,2:25],pch = 19, lower.panel = NULL)
-?pairs
+
+data.mat = data.matrix(review_data)
+data.mat = data.mat[,-1]
 
 ## feature combinations
 #aman
@@ -76,8 +75,10 @@ review_data %>%rename(
     avg_gardens = Category.24
 )
 
-c1  = review_data %>% select(Category.17, Category.18, Category.19)
+c1  = review_data %>% select(gym, bakeries, swimming_pool)
 c1 = data.matrix(c1)
+ggplot(c1, aes(x=swimming_pool, y=gym)) + 
+    geom_point(aes(color=bakeries))
 
 kmax=7
 silList=rep(0,kmax-1)
@@ -87,3 +88,82 @@ for(i in 2:kmax){
     silList[i-1] = mean(ss[, 3])
 }
 silList
+
+kmeans.out <- kmeans(c1,3,nstart=50,iter.max = 15)
+c1 = cbind(c1,kmeans.out$cluster)
+colnames(c1)[4] <- "cluster"
+c1[,4] = as.factor(c1[,4])
+ggplot(c1, aes(x=swimming_pool, y=gym, shape = cluster)) + 
+    geom_point(aes(color=bakeries))
+
+
+
+
+
+# view points, park, beach
+c1  = review_data %>% select(park, view_points, beach)
+#c1 = data.matrix(c1)
+ggplot(c1, aes(x=view_points, y=park)) + 
+    geom_point(aes(color=beach))
+kmax=7
+silList1=rep(0,kmax-1)
+for(i in 2:kmax){
+    kmeans.out <- kmeans(c1,i,nstart=50,iter.max = 15)
+    ss <- silhouette(kmeans.out$cluster, dist(data.mat))
+    silList1[i-1] = mean(ss[, 3])
+}
+silList1
+
+# theater, art galleries, cafe
+c1  = review_data %>% select(theatre, art_galleries, cafe)
+c1 = data.matrix(c1)
+kmax=7
+silList2=rep(0,kmax-1)
+for(i in 2:kmax){
+    kmeans.out <- kmeans(c1,i,nstart=50,iter.max = 15)
+    ss <- silhouette(kmeans.out$cluster, dist(data.mat))
+    silList2[i-1] = mean(ss[, 3])
+}
+silList2
+
+# restaurants, bar, local service
+c1  = review_data %>% select(restaurant, bar, local_service)
+c1 = data.matrix(c1)
+kmax=7
+silList3=rep(0,kmax-1)
+for(i in 2:kmax){
+    kmeans.out <- kmeans(c1,i,nstart=50,iter.max = 15)
+    ss <- silhouette(kmeans.out$cluster, dist(data.mat))
+    silList3[i-1] = mean(ss[, 3])
+}
+silList3
+
+
+c1  = review_data %>% select(beach,art_galleries)
+c1 = data.matrix(c1)
+kmax=7
+silList4=rep(0,kmax-1)
+for(i in 2:kmax){
+    kmeans.out <- kmeans(c1,i,nstart=50,iter.max = 15)
+    ss <- silhouette(kmeans.out$cluster, dist(data.mat))
+    silList4[i-1] = mean(ss[, 3])
+}
+silList4
+
+scatterplot3d(c1, pch = 16, angle = )
+
+ggplot(c1, aes(x=gym, y=bakeries)) + 
+    geom_point(aes(size=swimming_pool))
+
+ggplot(c1, aes(x=gym, y=bakeries)) + 
+    geom_point(aes(color=swimming_pool))
+
+
+## covarience matrix
+#install.packages("reshape2")
+library(reshape2)
+cormat = cor(data.mat,y=data.mat)
+melted_cormat <- melt(cormat)
+ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
+    geom_tile() + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 8, hjust = 1))
